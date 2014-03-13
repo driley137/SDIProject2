73c215446b460ac86ec7c40fca931a9a2378d47c
Dustin Riley <driley137@fullsail.edu>
2014-03-12 23:18:29 -0400

    Variables and first prompt
    Getting up to get a drink, so I’m saving.

--- riley_dustin_project2.js
+++ riley_dustin_project2.js
@@ -1,1 +1,12 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+/*
+  Dustin Riley
+  SDI 1403
+  Project 2
+*/
+
+//Declaring my global variables
+var fName = "Billy"; lName = "Belly"; time = 0; workout = true;
+
+fName = prompt("What is your first name?", "i.e. Gandalf");
